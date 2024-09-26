4ccd767619940f909be56857b9ffc81ad77bc8a1
tamdao <tam.dao@bnksolution.com>
2024-09-27 06:17:16 +0700

    u

--- Reservation.WEB/src/app/shared/modules/shared.module.ts
+++ Reservation.WEB/src/app/shared/modules/shared.module.ts
@@ -1,31 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {
-  NbCardModule,
-  NbInputModule,
-  NbLayoutModule,
-  NbButtonModule,
-  NbSelectModule,
-  NbCheckboxModule,
-  NbRadioModule,
-  NbAutocompleteModule,
-} from '@nebular/theme';
 
-export const libModule = [
-  NbInputModule,
-  NbSelectModule,
-  NbCardModule,
-  NbLayoutModule,
-  NbButtonModule,
-  NbCheckboxModule,
-  NbRadioModule,
-  NbAutocompleteModule,
-];
+export const libModule = [];
 export const customDirective = [];
 
 @NgModule({
   declarations: [],
+
   imports: [CommonModule, FormsModule, ReactiveFormsModule, ...libModule],
   exports: [CommonModule, FormsModule, ReactiveFormsModule, ...libModule],
   // providers: [NbStatusService], // Cung cấp service ở đây
