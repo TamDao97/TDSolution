ef4bc33b707d5fa5b9e52227761ead9e6e28296a
tamdao <tamdc8897@gmail.com>
2025-07-10 00:07:39 +0700

    u

--- Reservation.API/Program.cs
+++ Reservation.API/Program.cs
@@ -18,6 +18,9 @@ builder.Services.AddControllers(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+builder.WebHost.UseUrls($"http://*:{port}");
+
 var app = builder.Build();
 
 //Configure the HTTP request pipeline.
